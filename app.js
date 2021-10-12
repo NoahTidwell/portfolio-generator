5124fc17e984eb31124990adf665594b4a55109f
Noah Tidwell <ntidwell34@yahoo.com>
2021-10-11 22:42:14 -0500

    download inquirer npm and set up prompt

--- app.js
+++ app.js
@@ -1,38 +1,22 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+inquirer
+.prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'What is your name?'
+  }
+])
+.then(answers => console.log(answers));
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// const pageHTML = generatePage(name, github);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('index.html', pageHTML, err => {
+  // if (err) throw err;
 
-
-
-//const printProfileData = profileDataArr => {
-
- // profileDataArr.forEach(profileItem => console.log(profileItem));
-
-//}
-
-//printProfileData(profileDataArgs);
+  // console.log('Portfolio complete! Check out index.html to see the output!');
+// });
